43b03a9732374f2c788b6c43751af0fee25d400d
brittanymy <brittany.young2001@gmail.com>
2022-07-10 20:14:57 -0400

    added thought routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const {
+    allThoughts,
+    thoughtById,
+    addThought,
+    removeThought,
+    updateThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thought-controller');
+
+router.route('/').get(allThoughts);
+
+router.route('/:thoughtId').get(thoughtById);
+
+router.route('/:userId').post(addThought);
+
+router
+    .route('/:userId/thoughtId')
+    .delete(removeThought)
+    .put(updateThought);
+
+router
+    .route('/:userId/thoughtId/reactions')
+    .post(addReaction);
+
+router
+    .route('/:userId/:thoughtId/:reactionId')
+    .post(removeReaction);
+
+module.exports = router;
